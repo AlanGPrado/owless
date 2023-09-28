e23ac12fce24d41ed429b8673565f4604a90b8a1
Alan Prado <alangonzalezprado@gmail.com>
2023-09-28 03:23:49 -0600

    logo_dark_mode_support

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,11 +9,11 @@ import { ShareVariableService } from 'src/app/services/share-variable.service';
 })
 
 export class HeaderComponent implements OnInit {
-  customStyle: string | null = null;
   currentIcon: string = 'dark_mode';
   isCssApplied: boolean = false;
   mobileFlag: boolean = false;
   screenWidth: number = window.innerWidth;
+  imageSrc: string = '';
   items: { label: string; route: string; isActive: boolean }[] = [
     { label: 'Home', route: '/home', isActive: false },
     { label: 'Trabajos', route: '/trabajos', isActive: false },
@@ -44,15 +44,23 @@ export class HeaderComponent implements OnInit {
     })
   }
 
-  toggleIcon() {
+  toggleTheme() {
     (this.currentIcon === 'dark_mode') ? this.currentIcon = 'light_mode' : this.currentIcon = 'dark_mode';
-  }
-
-  toggleBackground() {
     this.isCssApplied = !this.isCssApplied;
     (this.isCssApplied) ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
     this.sharedService.toggleImage();
   }
+
+  // toggleIcon() {
+  //   (this.currentIcon === 'dark_mode') ? this.currentIcon = 'light_mode' : this.currentIcon = 'dark_mode';
+  // }
+
+  // toggleBackground() {
+  //   this.isCssApplied = !this.isCssApplied;
+  //   (this.isCssApplied) ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
+  //   this.sharedService.toggleImage();
+  //   console.log("CHINGASATUM")
+  // }
 
   constructor(private router: Router, private el: ElementRef, private sharedService: ShareVariableService) { }
 
@@ -85,6 +93,14 @@ export class HeaderComponent implements OnInit {
     }
     document.addEventListener('click', this.onDocumentClick.bind(this));
     console.log(this.router.url);
+    // Cambiar logo dark mode
+    this.sharedService.showImage$.subscribe((showImage) => {
+      if (showImage) {
+        this.imageSrc = 'assets/owless_logo_light.svg';
+      } else {
+        this.imageSrc = 'assets/owless_logo_dark.png';
+      }
+    });
   }
 
   ngOnDestroy() {
